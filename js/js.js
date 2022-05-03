b0c8a672751e7b70b2e52804bca1ce233fb5810e
victorcova <victor.avoc@gmail.com>
2022-05-03 17:12:53 -0300

    ajustes-finos-bootstrap

--- js/js.js
+++ js/js.js
@@ -7,4 +7,3 @@ var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggl
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
 })
-
